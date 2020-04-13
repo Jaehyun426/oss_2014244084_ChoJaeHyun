c6ec0e0ecde49a7c5464e47e52ae8588b55e5c1e
Jaehyun Cho <wogus753@naver.com>
2020-04-13 13:41:42 +0900

    Add add() function

--- helloworld.c
+++ helloworld.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+
+int add(int a, int b)
+{
+	return a+b;
+}
 void swap(int *a, int *b)
 {
 	int temp = *a;
@@ -6,11 +11,14 @@ void swap(int *a, int *b)
 	*b = temp;
 }
 
-
 int main()
 {
 
    printf("hello world!!!!\n");
+   
+   int a=3 , b=2;
+   printf("a+b = %d\n",add(a,b));
+
    return 0;
 
 }
